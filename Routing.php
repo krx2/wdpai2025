fb7b2d7cb9ca5c127a23c84eb2b73de5c73c03ee
Konrad <konrad.zebro@gmail.com>
2025-10-27 13:33:22 +0100

    lab4

---
+++ Routing.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'src/controllers/SecurityController.php';
+
+
+class Routing {
+
+    public static function run(string $path) {
+        //TODO na podstawie sciezki sprawdzamy jaki HTML zwrocic
+        switch ($path) {
+    case 'dashboard':
+        include 'public/views/dashboard.html';
+        break;
+    case 'login':
+        $controller = new SecurityController();
+        $controller->login();
+        // include 'public/views/login.html';
+        break;
+    default:
+        include 'public/views/404.html';
+        break;
+        } 
+    }
+}
