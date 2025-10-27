01eab07669e58a8a00363fc0b335fbbd6923fab7
Konrad <konrad.zebro@gmail.com>
2025-10-27 13:44:37 +0100

    routing

--- Routing.php
+++ Routing.php
@@ -7,17 +7,17 @@ class Routing {
     public static function run(string $path) {
         //TODO na podstawie sciezki sprawdzamy jaki HTML zwrocic
         switch ($path) {
-    case 'dashboard':
-        include 'public/views/dashboard.html';
-        break;
-    case 'login':
-        $controller = new SecurityController();
-        $controller->login();
-        // include 'public/views/login.html';
-        break;
-    default:
-        include 'public/views/404.html';
-        break;
+            case '':
+            case 'login':
+                $controller = new SecurityController();
+                $controller->login();
+                break;
+            case 'dashboard':
+                include 'public/views/dashboard.html';
+                break;
+            default:
+                include 'public/views/404.html';
+                break;
         } 
     }
 }
--- src/controllers/SecurityController.php
+++ src/controllers/SecurityController.php
@@ -2,20 +2,46 @@
 
 require_once 'AppController.php';
 
-
 class SecurityController extends AppController {
 
     public function login() {
-        // TODO get data from database
-
-        //  $this->render("login", ["name"=> "Adrian"]);
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $email = $_POST['email'] ?? '';
+            $password = $_POST['password'] ?? '';
+            
+            // TODO: validate and check credentials against database
+            // For now, just redirect to dashboard
+            header('Location: /dashboard');
+            exit();
+        }
+        
         return $this->render("login");
     }
 
     public function register() {
-        // TODO pobranie z formularza email i hasła
-        // TODO insert do bazy danych
-        // TODO zwrocenie informajci o pomyslnym zarejstrowaniu
-        return $this->render("login", ["message" => "Zarejestrowano uytkownika"]);
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $email = $_POST['email'] ?? '';
+            $password = $_POST['password'] ?? '';
+            $confirmPassword = $_POST['confirm_password'] ?? '';
+            
+            // TODO: Validate data
+            if (empty($email) || empty($password)) {
+                return $this->render("register", ["message" => "Wszystkie pola są wymagane"]);
+            }
+            
+            if ($password !== $confirmPassword) {
+                return $this->render("register", ["message" => "Hasła nie są zgodne"]);
+            }
+            
+            // TODO: Hash password
+            // $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+            
+            // TODO: Insert to database
+            
+            // Success - show message or redirect
+            return $this->render("login", ["message" => "Zarejestrowano użytkownika pomyślnie"]);
+        }
+        
+        return $this->render("register");
     }
 }
